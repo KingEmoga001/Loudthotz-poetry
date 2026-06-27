ba487ffecacac21548c6ea95a6cd760a844ddbdb
KingEmoga001 <danladiemoga@gmail.com>
2026-06-27 11:36:32 +0100

    git add package.json pnpm-workspace.yaml git commit -m "chore: migrate pnpm allowed builds to workspace config" git push

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,10 +1,1 @@
-module.exports = {
-  hooks: {
-    readPackage(pkg) {
-      if (['esbuild', '@firebase/util', 'protobufjs'].includes(pkg.name)) {
-        pkg.scripts = {};
-      }
-      return pkg;
-    }
-  }
-};
+module.exports = {};
