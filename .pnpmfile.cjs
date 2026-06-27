6cbf0164c0497efc6e84c840dd24eab1626a0337
KingEmoga001 <danladiemoga@gmail.com>
2026-06-27 11:42:09 +0100

    git add .pnpmfile.cjs git commit -m "fix: bypass esbuild postinstall execution on hostinger" git push

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,1 +1,13 @@
-module.exports = {};
+module.exports = {
+  hooks: {
+    readPackage(pkg) {
+      // Force pnpm to completely skip esbuild's broken postinstall script on Hostinger
+      if (pkg.name === 'esbuild') {
+        if (pkg.scripts && pkg.scripts.postinstall) {
+          delete pkg.scripts.postinstall;
+        }
+      }
+      return pkg;
+    }
+  }
+};
